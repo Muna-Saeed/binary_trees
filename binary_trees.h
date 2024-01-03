490dc9061c4cbb9fc5d28e14e56962a429cc92e4
abdiwoli <abdiwolih@gmail.com>
2024-01-03 23:56:18 +0300

    done

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -8,22 +10,42 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
+/**
+ * struct bst_t - Binary Search Tree node (alias for binary_tree_t)
+ */
 typedef struct binary_tree_s bst_t;
 
-typedef struct binary_tree_s bst_t;
-
+/**
+ * struct avl_t - AVL Tree node (alias for binary_tree_t)
+ */
 typedef struct binary_tree_s avl_t;
 
+/**
+ * struct heap_t - Binary Heap node (alias for binary_tree_t)
+ */
 typedef struct binary_tree_s heap_t;
 
+/**
+ * binary_tree_print - Prints a binary tree
+ * @tree: Pointer to the root node of the tree to print
+ */
 void binary_tree_print(const binary_tree_t *tree);
 
+/**
+ * binary_tree_node - Creates a new binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to put in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* BINARY_TREE_H */
