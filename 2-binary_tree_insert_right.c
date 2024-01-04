149df51e2c4cfcd4aee00a5966efd10dd5fdafa8
abdiwoli <abdiwolih@gmail.com>
2024-01-04 11:10:03 +0300

    try_fix2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * binary_tree_insert_right - insert right
@@ -10,18 +8,14 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
 
 
 	if (parent->right != NULL)
