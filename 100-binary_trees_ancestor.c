9978efc22906eb6c56cf33e080fee4b5fa60b9a5
abdiwoli <abdiwolih@gmail.com>
2024-01-05 16:51:50 +0300

    done

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - function name
+ * @first: fisrt node
+ * @second: second node.
+ * Return: common node
+ */
+
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *parent1, *parent2;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)second);
+
+	parent1 = first->parent;
+	parent2 = second->parent;
+	if (first == parent2 || parent1 == NULL || (parent1->parent == NULL && parent2))
+		return (binary_trees_ancestor(first, parent2));
+	else if (parent1 == second || parent2 == NULL || (parent2->parent == NULL && parent1))
+		return (binary_trees_ancestor(parent1, second));
+	return (binary_trees_ancestor(parent2, parent1));
+}
